74e541cf47101292ea95670fcb18610f171b347b
dolmaeng49 <dm49@naver.com>
2023-04-03 14:22:42 +0900

    배포 쉘스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ PROJECT_NAME=freelec-springboot-webservice
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-CURRENT_PID=$(pgrep -fl ${PROJECTNAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f ${PROJECTNAME})
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
