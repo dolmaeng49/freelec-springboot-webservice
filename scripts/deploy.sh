44090e770036a1578836110d36ef4cf55898aeec
dolmaeng49 <dm49@naver.com>
2023-04-03 14:02:43 +0900

    배포 쉘스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,6 +21,7 @@ echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 
 echo "> $JAR_NAME 에 실행권한 추가 및 실행"
+cd $REPOSITORY
 chmod +x $JAR_NAME
 
 nohup java -jar -Dspring.config.location=/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties,classpath:/application-real.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
